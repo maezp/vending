68259b03acb05e571be9efd069bf92f2ea8d9214
maezp <maezp09@gmail.com>
2023-02-16 11:05:28 +0100

    Display data from database

---
+++ src/ShowDevices.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const [devices, setDevices] = useState([
+  {
+    deviceAddress: "",
+    deviceLocation: "",
+    item1: "",
+    item2: "",
+    item3: "",
+    item4: "",
+  },
+]);
+
+useEffect(() => {
+  fetch("/device")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((jsonRes) => setDevices(jsonRes))
+    .catch((err) => console.log(err));
+}, [devices]);
+
+function ShowDevices(
+    {devices.map((device) => {
+    return (
+      <div key={device._id}>
+        <h1>{device.deviceAddress}</h1>
+        <h1>{device.deviceLocation}</h1>
+        <h1>{device.item1}</h1>
+        <h1>{device.item2}</h1>
+        <h1>{device.item3}</h1>
+        <h1>{device.item4}</h1>
+      </div>
+    );
+  })}
+  )
+export default function ShowDevices;
--- src/Tabela.js
+++ src/Tabela.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import axios from "axios";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 30 },
@@ -31,53 +32,7 @@ const columns: GridColDef[] = [
   },
 ];
 
-const rows = [
-  {
-    id: 1,
-    deviceAddress: "Azzurro",
-    deviceLocation: "Azzurro",
-    item1: 5,
-    item2: 4,
-    item3: 10,
-    item4: 4,
-  },
-  {
-    id: 2,
-    deviceAddress: "Craft",
-    deviceLocation: "Craft",
-    item1: 5,
-    item2: 4,
-    item3: 10,
-    item4: 4,
-  },
-  {
-    id: 3,
-    deviceAddress: "Royal",
-    deviceLocation: "Royal",
-    item1: 5,
-    item2: 4,
-    item3: 10,
-    item4: 4,
-  },
-  {
-    id: 4,
-    deviceAddress: "LaBocca ",
-    deviceLocation: "LaBocca",
-    item1: 5,
-    item2: 4,
-    item3: 10,
-    item4: 4,
-  },
-  {
-    id: 5,
-    deviceAddress: "Styx",
-    deviceLocation: "Styx",
-    item1: 5,
-    item2: 4,
-    item3: 10,
-    item4: 4,
-  },
-];
+const rows = [];
 
 export default function DataTable() {
   return (
